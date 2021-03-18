0f261f70377fa674697798297c7202b312b868e5
kenan1997-asac <kinant.s4@gmail.com>
2021-03-18 16:47:47 +0200

    added JS

---
+++ JavaScript/app.js
@@ -0,0 +1,15 @@
+alert ("HEllO EVs LOVERS");
+var favoritEV= prompt ("what's your favorit EV?");
+console.log(favoritEV);
+
+if (favoritEV==="Tesla"){
+    alert("press on first link");
+}
+else if (favoritEV==="Ford Mach-E")
+{alert("press on second link");}
+
+else if (favoritEV==="Mercedes-Benz EQC"){alert("press on third link");}
+
+else{alert("Choose your fav EV from the list below (Copy and Paste the name)");}
+
+document.write('<P> Welcome to the site!</P>');
