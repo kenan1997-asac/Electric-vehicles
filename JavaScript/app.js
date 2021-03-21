0cea5d87a02c0448ed509cfaa744e7e3e6e1d140
kenan1997-asac <kinant.s4@gmail.com>
2021-03-21 15:49:15 +0200

    added lab05

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,15 +1,38 @@
 alert ("HEllO EVs LOVERS");
-var favoritEV= prompt ("what's your favorit EV?");
-console.log(favoritEV);
 
-if (favoritEV==="Tesla"){
+var drivinglicense= prompt ("Do you have a driving licence?");
+while (drivinglicense!= "yes"){
+    drivinglicense=prompt("You should have a driving License");  
+}
+
+var favoritEV= prompt ("what's your favorit EV? Choose between  Tesla, Ford Mach-E, Mercedes-Benz EQC");
+
+
+while (!(favoritEV== "tesla" || favoritEV== "ford Mach-E" || "favoritEV== mercedes-Benz EQC")){
+
+if (favoritEV==="tesla"){
     alert("press on first link");
 }
-else if (favoritEV==="Ford Mach-E")
+else if (favoritEV==="ford Mach-E")
 {alert("press on second link");}
 
-else if (favoritEV==="Mercedes-Benz EQC"){alert("press on third link");}
-
-else{alert("Choose your fav EV from the list below (Copy and Paste the name)");}
+else if (favoritEV==="mercedes-Benz EQC"){alert("press on third link");} }
 
 document.write('<P> Welcome to the site!</P>');
+
+
+var rating= prompt("please rate our page from 1 to 5");
+while (rating>5){
+    rating=prompt("please rate our page from 1 to 5");
+}
+var ratingResult="";
+var stars="";
+if (rating>=0){
+var stars = '<img src="stars/star.png" width="250px">';}
+
+
+for(var i=0;i<rating;i++){
+    ratingResult+=stars
+}
+
+document.write(ratingResult);
