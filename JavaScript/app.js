3d82bd73609f013776be270a70f859d79917d9b6
kenan1997-asac <kinant.s4@gmail.com>
2021-03-22 17:00:12 +0200

    added css

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,38 +1,59 @@
-alert ("HEllO EVs LOVERS");
-
-var drivinglicense= prompt ("Do you have a driving licence?");
-while (drivinglicense!= "yes"){
-    drivinglicense=prompt("You should have a driving License");  
-}
-
-var favoritEV= prompt ("what's your favorit EV? Choose between  Tesla, Ford Mach-E, Mercedes-Benz EQC");
-
-
-while (!(favoritEV== "tesla" || favoritEV== "ford Mach-E" || "favoritEV== mercedes-Benz EQC")){
-
-if (favoritEV==="tesla"){
-    alert("press on first link");
-}
-else if (favoritEV==="ford Mach-E")
-{alert("press on second link");}
-
-else if (favoritEV==="mercedes-Benz EQC"){alert("press on third link");} }
+alert("HEllO EVs LOVERS");
 
 document.write('<P> Welcome to the site!</P>');
 
 
-var rating= prompt("please rate our page from 1 to 5");
-while (rating>5){
-    rating=prompt("please rate our page from 1 to 5");
-}
-var ratingResult="";
-var stars="";
-if (rating>=0){
-var stars = '<img src="stars/star.png" width="250px">';}
+var drivingLicense = prompt("Do you have a driving licence?");
 
-
-for(var i=0;i<rating;i++){
-    ratingResult+=stars
+function driving(userLincense) {
+    while (userLincense != "yes") {
+        userLincense = prompt("You should have a driving License");
+    }
 }
 
-document.write(ratingResult);
+driving(drivingLicense);
+
+
+var favoritEV = prompt("what's your favorit EV? Choose between  Tesla, Ford Mach-E, Mercedes-Benz EQC");
+
+function favorit(userFavorit) {
+    while (!(userFavorit == "tesla" || userFavorit == "ford Mach-E" || userFavorit == "mercedes-Benz EQC")) {
+
+        if (userFavorit === "tesla") {
+            alert("press on first link");
+        }
+        else if (userFavorit === "ford Mach-E") { alert("press on second link"); }
+
+        else if (userFavorit === "mercedes-Benz EQC") { alert("press on third link"); }
+    }
+}
+
+favorit(favoritEV);
+
+
+
+var rating = prompt("please rate our page from 1 to 5");
+
+
+var getRating=function () {
+    while (rating > 5) {
+        rating = prompt("please rate our page from 1 to 5");
+    }
+    var ratingResult = "";
+    var stars = "";
+    if (rating >= 0) {
+        var stars = '<img src="stars/star.png" width="25px">';
+    }
+    
+    
+    for (var i = 0; i < rating; i++) {
+        ratingResult += stars
+    }
+    return ratingResult;
+}
+
+
+
+
+
+ document.write(getRating());
